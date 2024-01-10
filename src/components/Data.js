c62330689035e62eeddf953a7ef27d656387e9fa
Sandhyayadv <111213114+Sandhyayadv@users.noreply.github.com>
2024-01-10 19:05:16 +0530

    Add files via upload

---
+++ src/components/Data.js
@@ -0,0 +1,319 @@
+const data=[
+    {id:1,
+    question:"Data can be permanentaly stored in ?",
+    answers:[
+      {
+        text:"RAM",
+        correct:false,
+      },
+      {
+        text:"Hard Disk",
+        correct:true,
+      },
+      {
+        text:"Cache Memory",
+        correct:false,
+      },
+      {
+        text:"Printer",
+        correct:false,
+      },
+    ],
+  },
+    {id:2,
+      question:"A bit can be ?",
+      answers:[
+        {
+          text:"1 and 0",
+          correct:false,
+        },
+        {
+          text:"1 or 0",
+          correct:true,
+        },
+        {
+          text:"1 Only",
+          correct:false,
+        },
+        {
+          text:"0 Only",
+          correct:false,
+        },
+      ],
+    },
+    {id:3,
+      question:"1 byte consist of ?",
+      answers:[
+        {
+          text:"4 bits",
+          correct:false,
+        },
+        {
+          text:" 8 bits ",
+          correct:true,
+        },
+        {
+          text:"16 bits",
+          correct:false,
+        },
+        {
+          text:"32 bits",
+          correct:false,
+        },
+      ],
+    },
+  
+    {id:4,
+      question:"Co-Founder of HyperVerge Academy ?",
+      answers:[
+        {
+          text:"Shaivya Sahare",
+          correct:false,
+        },
+        {
+          text:"Ruchira Ragavan",
+          correct:false,
+        },
+        {
+          text:"Gayathri Meka",
+          correct:true,
+        },
+        {
+          text:"Shruti Apala",
+          correct:false,
+        },
+      ],
+    },
+    {id:5,
+      question:"The Key starting with character F1 to F12 are ?",
+      answers:[
+        {
+          text:"Arrow Keys",
+          correct:false,
+        },
+        {
+          text:"Function Keys",
+          correct:true,
+        },
+        {
+          text:"Alphabet Keys",
+          correct:false,
+        },
+        {
+          text:"Numeric Keys",
+          correct:false,
+        },
+      ],
+    },
+    {id:6,
+      question:"For printing of large drawings and images we use ?",
+      answers:[
+        {
+          text:"Laser printing",
+          correct:false,
+        },
+        {
+          text:"Plotter",
+          correct:true,
+        },
+        {
+          text:"Line Printer",
+          correct:false,
+        },
+        {
+          text:"Dot Matrix printer",
+          correct:false,
+        },
+      ],
+    },
+    {id:7,
+      question:"What was the first internet search engine ?",
+      answers:[
+        {
+          text:"Yahoo",
+          correct:false,
+        },
+        {
+          text:"Google",
+          correct:false,
+        },
+        {
+          text:"Archie",
+          correct:true,
+        },
+        {
+          text:"Lycos",
+          correct:false,
+        },
+      ],
+    },
+    {id:8,
+      question:"Which as a bridge between the hardware and software ",
+      answers:[
+        {
+          text:"Operating System",
+          correct:true,
+        },
+        {
+          text:"Processor",
+          correct:false,
+        },
+        {
+          text:"RAM",
+          correct:false,
+        },
+        {
+          text:"CPU",
+          correct:false,
+        },
+      ],
+    },
+    {id:9,
+      question:"What is the name of first computer virus",
+      answers:[
+        {
+          text:"Creeper",
+          correct:true,
+        },
+        {
+          text:"Trojan",
+          correct:false,
+        },
+        {
+          text:"Rabbit",
+          correct:true,
+        },
+        {
+          text:"Mydoom",
+          correct:false,
+        },
+      ],
+    },
+    {id:10,
+      question:"Which computer program converts assembly language to machine language ?",
+      answers:[
+        {
+          text:"Assembler",
+          correct:true,
+        },
+        {
+          text:"Interpreter",
+          correct:false,
+        },
+        {
+          text:"Compiler",
+          correct:true,
+        },
+        {
+          text:"Comparator",
+          correct:false,
+        },
+      ],
+    },
+    {id:11,
+      question:"MS Excel is an example of ?",
+      answers:[
+        {
+          text:"Operating System",
+          correct:false,
+        },
+        {
+          text:"Application Software",
+          correct:true,
+        },
+        {
+          text:"Input Device",
+          correct:false,
+        },
+        {
+          text:"Processor",
+          correct:false,
+        },
+      ],
+    },
+    {id:12,
+      question:"Virus is a computer ? ",
+      answers:[
+        {
+          text:"Program",
+          correct:true,
+        },
+        {
+          text:"File",
+          correct:false,
+        },
+        {
+          text:"Document",
+          correct:false,
+        },
+        {
+          text:"Cookie",
+          correct:false,
+        },
+      ],
+    },
+    {id:13,
+      question:"What Operating System did google develop ? ",
+      answers:[
+        {
+          text:"Windows",
+          correct:false,
+        },
+        {
+          text:"Android",
+          correct:true,
+        },
+        {
+          text:"BlackBerry",
+          correct:false,
+        },
+        {
+          text:"OS iOS",
+          correct:false,
+        },
+      ],
+    },
+    {id:14,
+      question:"React  Js  is  a  ?",
+      answers:[
+        {
+          text:"Framework",
+          correct:false,
+        },
+        {
+          text:"Programming Language",
+          correct:false,
+        },
+        {
+          text:"Library",
+          correct:true,
+        },
+        {
+          text:"Web  Language",
+          correct:false,
+        },
+      ],
+    },
+    {id:15,
+      question:"React Js  is  developed  by ?",
+      answers:[
+        {
+          text:"Google",
+          correct:false,
+        },
+        {
+          text:"Facebook",
+          correct:true,
+        },
+        {
+          text:"Microsoft",
+          correct:false,
+        },
+        {
+          text:"Linked In",
+          correct:false,
+        },
+      ],
+    },
+  ];
+export default data; 
